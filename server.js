b6a53f1d506f66612a385549bfe4ae1f22910371
unknown <daniel@danielstern.ca>
2014-10-28 18:05:46 -0400

    parses-wikitext

--- server.js
+++ server.js
@@ -1,10 +1,53 @@
-var http = require('http');
+var http = require('http'),
+fs = require('fs');
 
 var server = http.createServer(function(req,res){
-	res.writeHead(200, {'Content-Type':'text/html'});
-	res.end("Hello world.");
-})
+	var url = require("url");
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+
+	if (query.text) {
+		res.writeHead(200, {'Content-Type':'text/html'});
+		res.write(JSON.stringify(getText()));
+		
+	}
+
+
+	
+
+	request.post(
+	    'http://www.yoursite.com/formpage',
+	    { form: { key: 'value' } },
+	    function (error, response, body) {
+	        if (!error && response.statusCode == 200) {
+	            console.log(body)
+	        }
+	    }
+	);
+
+	res.end();
+});
+
+
 
 var port = Number(process.env.PORT || 3000);
 
 server.listen(port);
+var request = require('request');
+
+function getText() {
+	var file = fs.readFileSync('example.wtxt');
+	var mainTitles = file.toString().split("'''").filter(function(elem,index){
+		return index % 2;
+	});
+
+	var subTitles = file.toString().split("''").filter(function(elem,index){
+		return index % 2;
+	});
+
+	return {
+		// file:file,
+		mainTitles:mainTitles,
+		subTitles:subTitles
+	};
+}
